dedecf21611eaebb83005cdd9e6cd71c4e5e8b0c
svitala500 <svitala500@gmail.com>
2019-08-10 10:54:20 +0700

    Добавлен калсс Raketa

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+class Raketa:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 3
+        self.dir = 0
+
+    def draw(self):
+        arcade.draw_ellipse_filled(self.x, self.y, 40, 60, [200, 0, 0])
 
 class MyGame(arcade.Window):
     """ Главный класс приложения. """
@@ -13,11 +22,13 @@ class MyGame(arcade.Window):
 
     def setup(self):
         # Настроить игру здесь
+        self.raketa = Raketa(200, 300)
         pass
 
     def on_draw(self):
         """ Отрендерить этот экран. """
         arcade.start_render()
+        self.raketa.draw()
         # Здесь код рисунка
 
     def update(self, delta_time):
