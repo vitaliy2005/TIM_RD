b7b8f550ee49291a8d964a6da1975ea3bf8d5107
svitala500 <svitala500@gmail.com>
2019-08-10 10:35:31 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class MyGame(arcade.Window):
+    """ Главный класс приложения. """
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def setup(self):
+        # Настроить игру здесь
+        pass
+
+    def on_draw(self):
+        """ Отрендерить этот экран. """
+        arcade.start_render()
+        # Здесь код рисунка
+
+    def update(self, delta_time):
+        """ Здесь вся игровая логика и логика перемещения."""
+        pass
+
+
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
